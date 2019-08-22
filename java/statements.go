c7cee14864beb56ee98c0e891bea0d4e9543be7d
Eugene Yao <eugene.yao@zocdoc.com>
2019-08-22 11:33:18 -0400

    Java enum generation (#5)
    * god help me
    
    * polish

---
+++ java/statements.go
@@ -0,0 +1,56 @@
+package java
+
+type StatementsDeclaration struct {
+	statements []Writable
+	isBlock    bool
+}
+
+func (self *StatementsDeclaration) AppendCode(code Writable) *StatementsDeclaration {
+	self.statements = append(self.statements, code)
+	return self
+}
+
+func (self *StatementsDeclaration) Append(code string) *StatementsDeclaration {
+	self.statements = append(self.statements, Code(code))
+	return self
+}
+
+func (self *StatementsDeclaration) Line(code string) *StatementsDeclaration {
+	self.
+		AppendCode(Code(code)).
+		AppendCode(Eol())
+	return self
+}
+
+func (self *StatementsDeclaration) Lines(lines ...string) *StatementsDeclaration {
+	for _, line := range lines {
+		self.Line(line)
+	}
+	return self
+}
+
+func (self *StatementsDeclaration) Block() *StatementsDeclaration {
+	body := Block()
+	self.AppendCode(body)
+	return body
+}
+
+func Statements() *StatementsDeclaration {
+	return &StatementsDeclaration{statements: []Writable{}, isBlock: false}
+}
+
+func Block() *StatementsDeclaration {
+	return &StatementsDeclaration{statements: []Writable{}, isBlock: true}
+}
+
+func (self *StatementsDeclaration) WriteCode(writer CodeWriter) {
+	if self.isBlock {
+		writer.Begin()
+	}
+	for _, line := range self.statements {
+		line.WriteCode(writer)
+	}
+	if self.isBlock {
+		writer.End()
+	}
+}
---
+++ java/writer.go
@@ -0,0 +1,59 @@
+package java
+
+import (
+	"strings"
+)
+
+type CodeWriter interface {
+	Begin()
+	End()
+	Eol()
+	Write(code string)
+}
+
+func prefix(indentation int) string {
+	tab := strings.Repeat(" ", 2)
+	return strings.Repeat(tab, indentation)
+}
+
+type codeWriter struct {
+	code        strings.Builder
+	indentation int
+	newLine     bool
+}
+
+func (self *codeWriter) Begin() {
+	self.Write(" {")
+	self.Eol()
+	self.indentation += 1
+}
+
+func (self *codeWriter) End() {
+	self.indentation -= 1
+	self.Write("}")
+	self.Eol()
+}
+
+func (self *codeWriter) Eol() {
+	if self.newLine {
+		self.newLine = false
+	}
+	self.Write("\n")
+	self.newLine = true
+}
+
+func (self *codeWriter) Write(code string) {
+	if self.newLine {
+		self.code.WriteString(prefix(self.indentation))
+		self.newLine = false
+	}
+	self.code.WriteString(code)
+}
+
+func (self *codeWriter) Code() string {
+	return self.code.String()
+}
+
+func CreateWriter() codeWriter {
+	return codeWriter{code: strings.Builder{}, indentation: 0, newLine: true}
+}
