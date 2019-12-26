82b9e45d5ba816179c4cb4ff6dfb6a6e9e7ce99a
Eugene Yao <eugene.yao@zocdoc.com>
2019-12-25 20:28:20 -0500

    Revert "Support param per line format"
    This reverts commit 28e79529cdadeb9b4d79a56b80253d892b526d02.

--- scala/method.go
+++ scala/method.go
@@ -11,7 +11,6 @@ type MethodDeclaration struct {
 	noParams       bool
 	implicitParams []Writable
 	definition     Writable
-	paramPerLine   bool
 }
 
 func (self *MethodDeclaration) Returns(returnType string) *MethodDeclaration {
@@ -64,11 +63,6 @@ func (self *MethodDeclaration) NoParams() *MethodDeclaration {
 	return self
 }
 
-func (self *MethodDeclaration) ParamPerLine() *MethodDeclaration {
-	self.paramPerLine = true
-	return self
-}
-
 func (self *MethodDeclaration) Define() *StatementsDeclaration {
 	statements := Statements()
 	self.definition = statements
@@ -100,7 +94,6 @@ func Method(name string) *MethodDeclaration {
 		params:         []Writable{},
 		implicitParams: []Writable{},
 		definition:     nil,
-		paramPerLine:   false,
 	}
 }
 
@@ -134,25 +127,11 @@ func (self *MethodDeclaration) WriteCode(writer CodeWriter) {
 
 	if !self.noParams {
 		writer.Write("(")
-		if self.paramPerLine {
-			writer.Indent()
-			writer.Eol()
-		}
 		for i, param := range self.params {
 			param.WriteCode(writer)
 			if i < len(self.params)-1 {
-				writer.Write(",")
+				writer.Write(", ")
 			}
-			if self.paramPerLine {
-				writer.Eol()
-			} else {
-				if i < len(self.params)-1 {
-					writer.Write(" ")
-				}
-			}
-		}
-		if self.paramPerLine {
-			writer.UnIndent()
 		}
 		writer.Write(")")
 	}
--- scala/method_test.go
+++ scala/method_test.go
@@ -14,29 +14,6 @@ func TestMethodZeroParams(t *testing.T) {
 	assertCode(t, Method("someMethod").NoParams(), `def someMethod`)
 }
 
-func TestMethodParams(t *testing.T) {
-	expected := `
-def someMethod(param1: String, param2: Int)
-`
-	method := Method("someMethod")
-	method.Param("param1", "String")
-	method.Param("param2", "Int")
-	assertCode(t, method, expected)
-}
-
-func TestMethodParamPerLine(t *testing.T) {
-	expected := `
-def someMethod(
-  param1: String,
-  param2: Int
-)
-`
-	method := Method("someMethod").ParamPerLine()
-	method.Param("param1", "String")
-	method.Param("param2", "Int")
-	assertCode(t, method, expected)
-}
-
 func TestMethodWithBody(t *testing.T) {
 	expected := `
 def someMethod() = {
