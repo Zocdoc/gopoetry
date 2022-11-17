aa50b497e56bc147dfdab713b357c1f565eecdef
Winston Riley <winston.riley@zocdoc.com>
2022-11-17 18:02:21 -0500

    feat: add typescript support for basic union types

---
+++ typescript/types.go
@@ -0,0 +1,57 @@
+package typescript
+
+import (
+	"fmt"
+)
+
+// DeclareType instantiates a new type declaration
+func DeclareType(name string, typeRef Writable) *TypeDeclaration {
+	return &TypeDeclaration{
+		name:          name,
+		typeReference: typeRef,
+	}
+}
+
+// TypeDeclaration declares a type
+type TypeDeclaration struct {
+	name          string
+	typeReference Writable
+}
+
+// WriteCode writes the type declaration to the writer
+func (td *TypeDeclaration) WriteCode(writer CodeWriter) {
+	writer.Eol()
+	writer.Write(fmt.Sprintf("type %s = ", td.name))
+	td.typeReference.WriteCode(writer)
+	writer.Write(";")
+	writer.Eol()
+}
+
+// UnionTypeRef represents the union of a series of type refs
+// see: https://www.typescriptlang.org/docs/handbook/typescript-in-5-minutes.html#unions
+type UnionTypeRef struct {
+	typeRefs []Writable
+}
+
+// UnionType is a constructor function for creating union types
+func UnionType(refs ...string) *UnionTypeRef {
+	typeRefs := []Writable{}
+
+	for _, str := range refs {
+		typeRefs = append(typeRefs, Code(str))
+	}
+
+	return &UnionTypeRef{
+		typeRefs: typeRefs,
+	}
+}
+
+// WriteCode writes the union type ref to the writer
+func (utr *UnionTypeRef) WriteCode(writer CodeWriter) {
+	for i, ref := range utr.typeRefs {
+		ref.WriteCode(writer)
+		if i < len(utr.typeRefs)-1 {
+			writer.Write(" | ")
+		}
+	}
+}
