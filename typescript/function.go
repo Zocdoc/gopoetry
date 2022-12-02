75a91536cd6a8286e0a07383be670ab48f249787
Winston Riley <winston.riley@zocdoc.com>
2022-12-02 10:37:42 -0500

    add support for arrow functions

--- typescript/function.go
+++ typescript/function.go
@@ -1,32 +1,87 @@
 package typescript
 
-type Function struct {
-	Name            string
-	body            BlockDeclaration
-	params          []ParamDeclaration
-	returnType      Writable
-	typeConstructor Writable
-	exported        bool
+// FunctionDecl represents a top level declaration.
+type FunctionDecl struct {
+	Name     string
+	exported bool
+	Function
 }
 
-func (f *Function) Export() *Function {
+// Export exports the function declaration
+func (f *FunctionDecl) Export() *FunctionDecl {
 	f.exported = true
 	return f
 }
 
-func (f *Function) AddParams(params ...ParamDeclaration) *Function {
+// AddParams add parameters to the function signiture
+func (f *FunctionDecl) AddParams(params ...ParamDeclaration) *FunctionDecl {
 	f.params = append(f.params, params...)
 	return f
 }
 
 // WriteCode implements Writable
-func (f *Function) WriteCode(writer CodeWriter) {
+func (f *FunctionDecl) WriteCode(writer CodeWriter) {
 	if f.exported {
 		writer.Write("export ")
 	}
 
-	writer.Write("function " + f.Name)
+	if f.async {
+		writer.Write("async ")
+	}
 
+	writer.Write("function " + f.Name)
+	f.writeCallSig(writer)
+	writer.Write(" ")
+	f.writeBody(writer)
+	writer.Eol()
+}
+
+// Function represents the function as an expression
+type Function struct {
+	async           bool
+	arrowSyntax     bool
+	body            BlockDeclaration
+	params          []ParamDeclaration
+	returnType      Writable
+	typeConstructor Writable
+}
+
+// Async marks the function as async
+func (f *Function) Async() *Function {
+	f.async = true
+	return f
+}
+
+// Arrow use arrow syntax for function
+func (f *Function) Arrow() *Function {
+	f.arrowSyntax = true
+	return f
+}
+
+// WriteCode implements Writable
+func (f *Function) WriteCode(writer CodeWriter) {
+	if f.async {
+		writer.Write("async ")
+	}
+
+	if !f.arrowSyntax {
+		writer.Write("function ")
+	}
+
+	f.writeCallSig(writer)
+
+	sep := " "
+	if f.arrowSyntax {
+		sep = " => "
+	}
+
+	writer.Write(sep)
+	f.writeBody(writer)
+}
+
+// writeCallSig writes functions call signature.
+// This includes any type constructors, parameters, and return types
+func (f *Function) writeCallSig(writer CodeWriter) {
 	if f.typeConstructor != nil {
 		writer.Write("<")
 		f.typeConstructor.WriteCode(writer)
@@ -46,12 +101,14 @@ func (f *Function) WriteCode(writer CodeWriter) {
 		writer.Write(": ")
 		f.returnType.WriteCode(writer)
 	}
+}
 
-	writer.Write(" ")
+// writeBody write function body
+func (f *Function) writeBody(writer CodeWriter) {
 	writer.OpenBlock()
 	for _, ln := range f.body.lines {
 		ln.WriteCode(writer)
 		writer.Eol()
 	}
-	writer.End()
+	writer.CloseBlock()
 }
--- typescript/function_test.go
+++ typescript/function_test.go
@@ -6,9 +6,9 @@ func TestFunction(t *testing.T) {
 	expected := `
 function myFunction() {
 }
-	`
+  `
 
-	fn := &Function{
+	fn := &FunctionDecl{
 		Name: "myFunction",
 	}
 
@@ -19,13 +19,15 @@ func TestFunctionWithParams(t *testing.T) {
 	expected := `
 function add(x: number, y: number) {
 }
-	`
+  `
 
-	fn := &Function{
+	fn := &FunctionDecl{
 		Name: "add",
-		params: []ParamDeclaration{
-			*Param("number", "x"),
-			*Param("number", "y"),
+		Function: Function{
+			params: []ParamDeclaration{
+				*Param("number", "x"),
+				*Param("number", "y"),
+			},
 		},
 	}
 
@@ -36,11 +38,29 @@ func TestReturnType(t *testing.T) {
 	expected := `
 function add(): void {
 }
-	`
+  `
 
-	fn := &Function{
-		Name:       "add",
-		returnType: Code("void"),
+	fn := &FunctionDecl{
+		Name: "add",
+		Function: Function{
+			returnType: Code("void"),
+		},
+	}
+
+	assertCode(t, fn, expected)
+}
+
+func TestAsyncFuncDecl(t *testing.T) {
+	expected := `
+async function asyncFunc() {
+}
+`
+
+	fn := &FunctionDecl{
+		Name: "asyncFunc",
+		Function: Function{
+			async: true,
+		},
 	}
 
 	assertCode(t, fn, expected)
@@ -50,11 +70,13 @@ func TestExportedFn(t *testing.T) {
 	expected := `
 export function add(): void {
 }
-	`
+  `
 
-	fn := &Function{
-		Name:       "add",
-		returnType: Code("void"),
+	fn := &FunctionDecl{
+		Name: "add",
+		Function: Function{
+			returnType: Code("void"),
+		},
 	}
 
 	fn.Export()
@@ -67,18 +89,20 @@ func TestBody(t *testing.T) {
 export function add(x: number, y: number): number {
     return x + y;
 }
-	`
+  `
 
-	fn := &Function{
+	fn := &FunctionDecl{
 		Name: "add",
-		params: []ParamDeclaration{
-			*Param("number", "x"),
-			*Param("number", "y"),
-		},
-		returnType: Code("number"),
-		body: BlockDeclaration{
-			lines: []Writable{
-				Code("return x + y;"),
+		Function: Function{
+			params: []ParamDeclaration{
+				*Param("number", "x"),
+				*Param("number", "y"),
+			},
+			returnType: Code("number"),
+			body: BlockDeclaration{
+				lines: []Writable{
+					Code("return x + y;"),
+				},
 			},
 		},
 	}
@@ -93,20 +117,159 @@ func TestTypeConstructor(t *testing.T) {
 function id<T>(thing: T): T {
     return thing;
 }
-	`
+  `
 
-	fn := &Function{
-		Name:            "id",
-		typeConstructor: Code("T"),
-		params: []ParamDeclaration{
-			*Param("T", "thing"),
-		},
-		returnType: Code("T"),
-		body: BlockDeclaration{
-			lines: []Writable{
-				Code("return thing;"),
+	fn := &FunctionDecl{
+		Name: "id",
+		Function: Function{
+			typeConstructor: Code("T"),
+			params: []ParamDeclaration{
+				*Param("T", "thing"),
+			},
+			returnType: Code("T"),
+			body: BlockDeclaration{
+				lines: []Writable{
+					Code("return thing;"),
+				},
 			},
 		},
 	}
 	assertCode(t, fn, expected)
+}
+
+func TestFunctionExpression(t *testing.T) {
+	expected := `
+const myFunc = function () {
+    return 42;
+};
+`
+	functionExperssion := &Function{
+		body: BlockDeclaration{
+			lines: []Writable{
+				Code("return 42;"),
+			},
+		},
+	}
+
+	funcConst := Const("myFunc", nil, functionExperssion)
+	assertCode(t, funcConst, expected)
+}
+
+func TestFunctionExpressionWithParams(t *testing.T) {
+	expected := `
+const add = function (a: number, b: number) {
+    return a + b;
+};
+`
+	functionExperssion := &Function{
+		params: []ParamDeclaration{
+			*Param("number", "a"),
+			*Param("number", "b"),
+		},
+		body: BlockDeclaration{
+			lines: []Writable{
+				Code("return a + b;"),
+			},
+		},
+	}
+
+	funcConst := Const("add", nil, functionExperssion)
+	assertCode(t, funcConst, expected)
+}
+
+func TestFunctionExpressionWithReturnValue(t *testing.T) {
+	expected := `
+const add = function (a: number, b: number): number {
+    return a + b;
+};
+`
+	functionExperssion := &Function{
+		params: []ParamDeclaration{
+			*Param("number", "a"),
+			*Param("number", "b"),
+		},
+		returnType: Code("number"),
+		body: BlockDeclaration{
+			lines: []Writable{
+				Code("return a + b;"),
+			},
+		},
+	}
+
+	funcConst := Const("add", nil, functionExperssion)
+	assertCode(t, funcConst, expected)
+}
+
+func TestFunctionExpressionWithTypeConstructor(t *testing.T) {
+	expected := `
+const id = function <T>(x: T): T {
+    return x;
+};
+`
+	functionExperssion := &Function{
+		typeConstructor: Code("T"),
+		params: []ParamDeclaration{
+			*Param("T", "x"),
+		},
+		returnType: Code("T"),
+		body: BlockDeclaration{
+			lines: []Writable{
+				Code("return x;"),
+			},
+		},
+	}
+
+	funcConst := Const("id", nil, functionExperssion)
+	assertCode(t, funcConst, expected)
+}
+
+func TestAddArrowFunction(t *testing.T) {
+	expected := `
+const f = <T>(_x: T): number => {
+    return 2;
+};
+`
+
+	functionExperssion := &Function{
+		typeConstructor: Code("T"),
+		arrowSyntax:     true,
+		params: []ParamDeclaration{
+			*Param("T", "_x"),
+		},
+		returnType: Code("number"),
+		body: BlockDeclaration{
+			lines: []Writable{
+				Code("return 2;"),
+			},
+		},
+	}
+
+	funcConst := Const("f", nil, functionExperssion)
+	assertCode(t, funcConst, expected)
+}
+
+func TestAddArrowFuncEmptyThunk(t *testing.T) {
+	expected := `
+const f = () => {
+};
+`
+
+	functionExperssion := &Function{}
+	functionExperssion.Arrow()
+
+	funcConst := Const("f", nil, functionExperssion)
+	assertCode(t, funcConst, expected)
+}
+
+func TestAsyncArrowFuncEmptyThunk(t *testing.T) {
+	expected := `
+const asyncF = async () => {
+};
+`
+
+	functionExperssion := &Function{}
+	functionExperssion.Arrow()
+	functionExperssion.Async()
+	funcConst := Const("asyncF", nil, functionExperssion)
+	assertCode(t, funcConst, expected)
 }
