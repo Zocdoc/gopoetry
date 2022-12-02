5b846c17d2d1873e91c76c9bcfed9b949a9277ed
Winston Riley <winston.riley@zocdoc.com>
2022-12-02 14:42:08 -0500

    add if block

--- typescript/if_statement.go
+++ typescript/if_statement.go
@@ -1,6 +1,37 @@
 package typescript
 
-type IfBlock struct {
-	condition          Writable
-	ifBlock, elseBlock BlockDeclaration
+// IfStatement represents an if/else statement block
+type IfStatement struct {
+	condition Writable
+	ifBlock   OTBSBlock
+	elseBlock *OTBSBlock
+}
+
+func If(cond Writable) *IfStatement {
+	return &IfStatement{condition: cond}
+}
+
+func (ifB *IfStatement) IfBlockAppend(lines ...Writable) *IfStatement {
+	ifB.ifBlock.lines = append(ifB.ifBlock.lines, lines...)
+	return ifB
+}
+
+func (ifB *IfStatement) ElseBlockAppend(lines ...Writable) *IfStatement {
+	if ifB.elseBlock == nil {
+		ifB.elseBlock = &OTBSBlock{}
+	}
+	ifB.elseBlock.lines = append(ifB.elseBlock.lines, lines...)
+	return ifB
+}
+
+func (ifS *IfStatement) WriteCode(writer CodeWriter) {
+	writer.Write("if (")
+	ifS.condition.WriteCode(writer)
+	writer.Write(") ")
+	ifS.ifBlock.WriteCode(writer)
+
+	if ifS.elseBlock != nil {
+		writer.Write(" else ")
+		ifS.elseBlock.WriteCode(writer)
+	}
 }
