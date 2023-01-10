55f618c71f8a1f33ffc6355b687036f9db85c611
Vito Laurenza <vito.laurenza@zocdoc.com>
2023-01-10 16:19:32 -0500

    feat: add support for calling a base class ctor

---
+++ csharp/base.go
@@ -0,0 +1,22 @@
+package csharp
+
+type BaseStatement struct {
+	arguments []string
+}
+
+func Base(args []string) *BaseStatement {
+	return &BaseStatement{
+		arguments: args,
+	}
+}
+
+func (self *BaseStatement) WriteCode(writer CodeWriter) {
+	writer.Write(" : base(")
+	for i, arg := range self.arguments {
+		writer.Write(arg)
+		if i < len(self.arguments)-1 {
+			writer.Write(", ")
+		}
+	}
+	writer.Write(")")
+}
