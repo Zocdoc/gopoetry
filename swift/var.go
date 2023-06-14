46356cc56ac12ee9e7d08dd146ba4478d7c4314b
Winston Riley <winston.riley@zocdoc.com>
2023-06-14 11:27:45 -0400

    Add more modifiers

--- swift/var.go
+++ swift/var.go
@@ -3,8 +3,10 @@ package swift
 import "fmt"
 
 type VarDeclaration struct {
-	name, typeName, accessModifier string
-	initValue                      Writable
+	accessModifier string
+	name           string
+	typeName       string
+	initValue      Writable
 }
 
 // VarDeclaration implements Declaration.
@@ -26,6 +28,21 @@ func (v *VarDeclaration) InitWith(value Writable) *VarDeclaration {
 
 func (v *VarDeclaration) Public() *VarDeclaration {
 	v.accessModifier = "public"
+	return v
+}
+
+func (v *VarDeclaration) Internal() *VarDeclaration {
+	v.accessModifier = "internal"
+	return v
+}
+
+func (v *VarDeclaration) FilePrivate() *VarDeclaration {
+	v.accessModifier = "fileprivate"
+	return v
+}
+
+func (v *VarDeclaration) Private() *VarDeclaration {
+	v.accessModifier = "private"
 	return v
 }
 
--- swift/var_test.go
+++ swift/var_test.go
@@ -1,6 +1,9 @@
 package swift
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestVar(t *testing.T) {
 	assertCode(t, Var("name", "String"), "var name: String")
@@ -24,4 +27,28 @@ var name: String
 var count: Int
 `
 	assertCode(t, unit, expected)
+}
+
+func TestValAccessModifiers(t *testing.T) {
+	myVal := Var("myVal", "Any")
+
+	modifiers := []func() *VarDeclaration{
+		myVal.Public,
+		myVal.FilePrivate,
+		myVal.Private,
+		myVal.Internal,
+	}
+
+	expected := []string{
+		"public",
+		"fileprivate",
+		"private",
+		"internal",
+	}
+
+	for i, modify := range modifiers {
+		modify()
+		expected := fmt.Sprintf("%s var myVal: Any", expected[i])
+		assertCode(t, myVal, expected)
+	}
 }
